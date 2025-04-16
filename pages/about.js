e636cc44da49eb23831c09f0d235f1f477621561
parv9999 <parvchauhan36@gmail.com>
2025-04-16 22:42:15 +0530

    Add files via upload

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+// pages/about.js
+
+export default function About() {
+  return (
+    <div>
+      <h1>About EcoArch</h1>
+      <p>EcoArch is a sustainability-driven project tackling urban heat using green roofs.</p>
+    </div>
+  );
+}
